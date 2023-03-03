f14d810a74348e4651ec7eca06b3484256ae3cda
20urc3-0FFTOPIC <126904597+20urc3-0FFTOPIC@users.noreply.github.com>
2023-03-03 19:51:30 +0100

    compare biggest line between char array

--- exercices/ch6.c
+++ exercices/ch6.c
@@ -22,3 +22,25 @@ int main(){
   }
   return 0;
 }
+
+int readline(char s[], int lim){
+  int c, i;
+  for(i=0; i< lim-1 && (c=getchar())!=EOF && c!='\n'; i++){
+    s[i]=c;
+  }
+  if (c=='\n'){
+    s[i]=c;
+    ++i;
+  }
+  s[i] = '\0';
+  return i;
+}
+
+void copy(char to[], char from[]){
+  int i;
+  
+  i=0;
+  while((to[i] = from[i]) != '\0'){
+    ++i;
+  }
+}
